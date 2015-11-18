0c6203e696080b94e0c9d22403ac089598737125
Balint Soos <soosbalint95@gmail.com>
2015-11-18 11:49:28 +0100

    express

--- Lesson-11/server/server.js
+++ Lesson-11/server/server.js
@@ -1,8 +1,25 @@
-var http = require('http');
+// var http = require('http');
+
+// var port = process.env.PORT || 3000;
+
+// http.createServer(function (req, res) {
+// 	res.writeHead(200, {'Content-Type': 'text/plain'});
+// 	res.end('Hello browser!\n');
+// }).listen(port);
+
+var express = require('express');
+var app = express();
+
+app.all('*', function (req, res, next) {
+	console.log(req.method, req.url);
+	next();
+});
+
+app.get('/', function (req, res) {
+	res.send("Hello client");
+});
 
 var port = process.env.PORT || 3000;
-
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello browser!\n');
-}).listen(port);
+app.listen(port, function () {
+	console.log("Listening...");
+});
